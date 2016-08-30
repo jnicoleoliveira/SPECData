142882807e7964aaab89985bc2c609f8f5e7d31b
joli <jnicoleoiveria@gmail.com>
2016-08-30 14:42:48 -0400

    implemented splashscreen-progress bar

--- app/dialogs/dialog___experiment_view.py
+++ app/dialogs/dialog___experiment_view.py
@@ -4,16 +4,19 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.uic import loadUi
-from frames.frame___experiment import Ui_Dialog
-
-#from frames.frame___experiment_view import Ui_ExperimentDialog  # Import frame
-#from ..experiment_analysis import ExperimentGraph
-from widget___molecule_selection import MoleculeSelectionWidget
-
 import pyqtgraph as pg
+
+from frames.frame___experiment import Ui_Dialog
+from widget___molecule_selection import MoleculeSelectionWidget
+from splash_screens import LoadingProgressScreen
+from ..experiment_analysis import ExperimentGraph
+from functions import experiment
+from config import resources
+
+import time
 import numpy as np
 import os
-from functions import experiment
+
 #Ui_Experiment = \
 #    loadUi(os.path.join(os.path.dirname(__file__), 'frames', 'experiment.ui'))
 
@@ -26,14 +29,65 @@ class ExperimentView(QDialog):
         self.ui.setupUi(self)
         self.setAttribute(Qt.WA_DeleteOnClose)
         self.setWindowTitle("Experiment View")
+        self.plot_widget = None
+        self.selection_widget = None
 
-        self.experiment = experiment.Experiment(experiment_name, mid)
+        self.experiment = None
+        self.loading_screen = None
 
-        # Functions
-        self.do_analysis()
-        self.setup_layout()
+        self.startup(experiment_name, mid)
 
-        self.show()
+    def startup(self, experiment_name, mid):
+        self.loading_screen = LoadingProgressScreen()
+        self.loading_screen.start()     # Start Loading Screen
+
+        ## Do Things ##
+
+        # Create Experiment
+        self.loading_screen.set_caption('Creating experiment...')
+        self.experiment = self.create_experiment(experiment_name, mid)  # Create experiment obj
+        time.sleep(1)
+        self.loading_screen.next_value(20)      # Progress ++
+
+        # Analyze Experiment
+        self.loading_screen.set_caption('Analyzing...')
+        self.do_analysis()                      # Run Analysis
+        self.loading_screen.next_value(40)      # Progress ++
+        time.sleep(3)
+
+        # Setup Layout
+        self.loading_screen.set_caption('Setting up...')
+        self.setup_layout()                     # Setup Layout
+        self.loading_screen.next_value(60)      # Progress ++
+        self.add_selection_assignments()        # Add assignments
+        time.sleep(2)
+        self.loading_screen.next_value(80)      # Progress ++
+        self.graph()
+        time.sleep(2)
+
+        self.loading_screen.end()
+
+    def create_experiment(self, experiment_name, mid):
+        return experiment.Experiment(experiment_name, mid)
+
+    def start_splash_screen(self):
+        from config import resources
+        pix_map = QPixmap(os.path.join(resources, 'specdata_logo.png'))
+        self.splash_screen = QSplashScreen(pix_map)
+
+        progressBar = QProgressBar(self.splash_screen)
+        progressBar.setGeometry(self.splash_screen.width()/10, 7*self.splash_screen.height()/10, 8*self.splash_screen.width()/10, self.splash_screen.height()/10)
+        self.splash_screen.show()
+
+        #for i in range (0,100):
+        #    progressBar.setValue(i)
+
+            #Analysize
+        #self.splash_screen.setPixmap(pix_map, Qt.WindowStaysOnTopHint)
+        #self.splash_screen.show()
+
+    def stop_splash_screen(self):
+        self.splash_screen.close()
 
     def do_analysis(self):
         self.experiment.get_assigned_molecules()
@@ -45,19 +99,23 @@ class ExperimentView(QDialog):
         self.setLayout(layout)
 
         # Widgets
-        plot_widget = pg.PlotWidget()
-        selection_widget = MoleculeSelectionWidget()
+        self.plot_widget = pg.PlotWidget()
+        self.selection_widget = MoleculeSelectionWidget()
         #spacer1_widget = QSpacerItem()
 
-        # Set
-        selection_widget.add_all(self.experiment.get_assigned_names(), self.experiment.get_assigned_mids())
-        print "added!"
         ## Add Widgets to layout
-        layout.addWidget(selection_widget, 0,0)
+        layout.addWidget(self.selection_widget, 0,0)
         #layout.addWidget(spacer1_widget, 0, 1)
-        layout.addWidget(plot_widget, 0,2)
+        layout.addWidget(self.plot_widget, 0,2)
 
-        
+    def add_selection_assignments(self):
+        # Set
+        self.selection_widget.add_all(self.experiment.get_assigned_names(), self.experiment.get_assigned_mids())
+
+    def graph(self):
+        experiment_graph = ExperimentGraph(self.experiment, self.selection_widget, [self.plot_widget,0])
+        experiment_graph.graph_assignments(0)
+
     def connect_buttons(self):
         redisplay_btn = self.ui.redisplay_btn
 

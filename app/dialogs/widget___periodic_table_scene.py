6297f0894d1d0113a1061084643368701c0e888d
joli <jnicoleoiveria@gmail.com>
2016-12-02 14:01:24 -0500

    - implemented periodic_table_scene to be changed into individual HoverButtons, that perform an action when hovered - made UI changes, by removing spacer items, and instead adjusting the graphitem's margin size

--- app/dialogs/widget___periodic_table_scene.py
+++ app/dialogs/widget___periodic_table_scene.py
@@ -13,15 +13,69 @@ class PeriodicTableSceneWidget(QWidget):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
         self.setMouseTracking(True)
-        #QObject.connect(self.ui.Li, SIGNAL('hovered()'), self.label_EnterEvent)
-        #self.ui.Li.setMouseTracking(True)
-        #hoverable(self.ui.Li)
-        self.ui.pushButton.setMouseTracking(True)
-        self.ui.pushButton = HoverButton()
-    def label_EnterEvent(self):
-        print("Enter")
-        self.setStyleSheet("background-color:#45b545;")
+        self.delete_widgets = []
 
+        self.transfer_labels_to_buttons(self.ui.periodic_grid_1)
+        self.transfer_labels_to_buttons(self.ui.periodic_grid_2)
+
+    def transfer_labels_to_buttons(self, grid):
+        count = grid.count()
+
+        items = (grid.itemAt(i) for i in range(count))
+
+
+        i = 0
+        none_count = 0
+        none_count = 0
+        for w in items:
+            if w.widget() is not None and not isinstance(w.widget(), QSpacerItem):
+                print w.widget().objectName()
+                print w.widget().text()
+                r, c, cs, rs = grid.getItemPosition(i)
+                self.set_next(grid, w.widget(), r, c)
+                i += 1
+                print i
+                print "\n"
+                #print count
+            else:
+                none_count += 1
+
+        print none_count
+        self.clear_remove_later(grid)
+
+    def set_next(self, grid, button, x, y):
+
+
+        # Get Button Attributes
+        #x = button.x()
+        #y = button.y()
+        print str(x) + " " + str(y)
+        #print button.text()
+        text = button.text()
+        stylesheet = button.styleSheet() + " border: 1px solid white; "
+        size_policy = button.sizePolicy()
+
+        # Create New Button
+        new_button = HoverButton()
+        new_button.setText(text)
+        new_button.setSizePolicy(size_policy)
+        new_button.setStyleSheet(stylesheet)
+        #print stylesheet
+        new_button.setMinimumSize(40, 40)
+        new_button.setMaximumSize(40, 40)
+        new_button.setAutoRaise(True)
+
+        self.delete_widgets.append(button)
+        grid.addWidget(new_button, x, y)
+
+    def clear_remove_later(self, grid):
+        for r in self.delete_widgets:
+            grid.removeWidget(r)
+            r.deleteLater()
+            r.close()
+            del r
+
+        self.delete_widgets = []
 
 def hoverable(widget):
     class Filter(QObject):
@@ -52,10 +106,16 @@ class HoverLabel(QLabel):
         self.setStyleSheet("background-color:yellow;")
         print("Leave")
 
-class HoverButton(QPushButton):
+class HoverButton(QToolButton):
+
     def __init__(self, parent=None):
-        QPushButton.__init__(self, parent)
+        super(HoverButton, self).__init__(parent)
         self.setMouseTracking(True)
 
-    def mouseMoveEvent(self, event):
-        print 'Mouse moved!'
+    def enterEvent(self,event):
+        print("Enter")
+        self.setStyleSheet("background-color:#45b545;")
+
+    def leaveEvent(self,event):
+        self.setStyleSheet("background-color:yellow;")
+        print("Leave")

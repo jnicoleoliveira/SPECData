7214fce63e9c325057c294655f6649a181f8a4c5
joli <jnicoleoiveria@gmail.com>
2017-04-06 16:15:09 -0400

    -- removed [stub] indecator for assignment view info widget notes

--- app/dialogs/widgets/widget___assignment_window_info.py
+++ app/dialogs/widgets/widget___assignment_window_info.py
@@ -6,6 +6,7 @@ from PyQt4.QtGui import *
 import tables.peaks_table as peaks_table
 from app.dialogs.frames.assignment_view.frame___assignment_info_widget import Ui_Form
 from config import conn
+from tables.knowninfo_table import get_notes
 
 
 class AssignmentInfoWidget(QWidget):
@@ -26,8 +27,16 @@ class AssignmentInfoWidget(QWidget):
         self.ui.peaks_assigned_val.setText(str(len(self.match.matches)))
         self.ui.total_peaks_val.setText(str(peaks_table.get_peak_count(conn, self.match.mid)))
         self.ui.presence_val_lbl.setText(str(self.match.p * 100))
-        self.ui.info_val_lbl.setText("[stub]")
+        self.set_info_text()
         self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+
+    def set_info_text(self):
+        mid = self.match.mid
+
+        try:
+            self.ui.info_val_lbl.setText(get_notes(conn, mid))
+        except TypeError:
+            self.ui.info_val_lbl.setText("(Splatalogue Entry)")
 
     def set_name(self, text):
         self.ui.experiment_name_lbl.setText(text)

12285c349d5055a0eeedb730dbe020533148a38b
joli <jnicoleoiveria@gmail.com>
2017-05-18 16:30:47 -0400

    -- added windows-setup.cmd  (NOT TESTED YET) -- added resources/screenshots folder for tutorials

--- init/windows-setup.cmd
+++ init/windows-setup.cmd
@@ -1,14 +1,13 @@
-#!/usr/bin/env bash
-# linux-setup.cmd
-# Jasmine N. Oliveira
-# Installs the SPECdata environment in Anaconda
-# and opens the setup Wizard for configuration
+:: windows-setup.cmd
+:: Jasmine N. Oliveira
+:: Installs the SPECdata environment,
+:: and opens the Setup Wizard for configuration
 
 echo "Installing Conda Environment..."
 conda env create -n specdata-env -f bin/conda_environment_linux.yml
 
 echo "Open Environment.."
-source activate specdata-env
+activate specdata-env
 
 echo "Running Setup Wizard..."
 cd ..

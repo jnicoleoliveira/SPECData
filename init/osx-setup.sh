9ce967613cfefd015567c0cd088efde48e6836b2
joli <jnicoleoiveria@gmail.com>
2017-05-25 14:19:24 -0400

    -- ommited "env" tag when creating a conda enviornment for OSX installer

--- init/osx-setup.sh
+++ init/osx-setup.sh
@@ -6,11 +6,13 @@
 # and opens the setup Wizard for configuration
 
 echo "Installing Conda Environment..."
-conda env create -n specdata-env -f bin/conda_environment_osx.yml
+conda create -n specdata-env -f bin/conda_environment_osx.yml
 
+echo " "
 echo "Open Environment.."
 source activate specdata-env
 
+echo " "
 echo "Running Setup Wizard..."
 cd ..
 python run_wizard.py
